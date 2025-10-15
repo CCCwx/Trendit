9304a48052564e67e2ddb1dfa0c5c26a9298b358
wuxi <vcwx329@gmail.com>
2025-10-15 10:25:27 -0700

    Layout-Structure Created and  Style reset

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { RouterProvider } from 'react-router-dom'
 import router from './router/index.jsx'
 import { Provider } from 'react-redux'
 import store from './store'
-
+import 'normalize.css'
 createRoot(document.getElementById('root')).render(
   <Provider store = {store}>
     <RouterProvider router= {router}/>
--- src/page/Layout/index.jsx
+++ src/page/Layout/index.jsx
@@ -1,16 +1,60 @@
-// 测试token是否成功注入
-import { request } from "@/utils"
-import { useEffect } from "react"
-const Layout = () =>{
-    useEffect(()=>{
-        request.get('/user/profile')
-    }, [])
-    
-    return(
-        <>  
-        this is Layout
-        </>
-    )
-}
+import { Layout, Menu, Popconfirm } from 'antd'
+import {
+  HomeOutlined,
+  DiffOutlined,
+  EditOutlined,
+  LogoutOutlined,
+} from '@ant-design/icons'
+import './index.scss'
 
-export default Layout 
+const { Header, Sider } = Layout
+
+const items = [
+  {
+    label: 'Home Page',
+    key: '1',
+    icon: <HomeOutlined />,
+  },
+  {
+    label: 'Blog Management',
+    key: '2',
+    icon: <DiffOutlined />,
+  },
+  {
+    label: 'Create Blog',
+    key: '3',
+    icon: <EditOutlined />,
+  },
+]
+
+const GeekLayout = () => {
+  return (
+    <Layout>
+      <Header className="header">
+        <div className="logo" />
+        <div className="user-info">
+          <span className="user-name">Wuxi</span>
+          <span className="user-logout">
+            <Popconfirm title="Confirm Logout?" okText="Logout" cancelText="Cancel">
+              <LogoutOutlined /> Logout
+            </Popconfirm>
+          </span>
+        </div>
+      </Header>
+      <Layout>
+        <Sider width={200} className="site-layout-background">
+          <Menu
+            mode="inline"
+            theme="dark"
+            defaultSelectedKeys={['1']}
+            items={items}
+            style={{ height: '100%', borderRight: 0 }}></Menu>
+        </Sider>
+        <Layout className="layout-content" style={{ padding: 20 }}>
+          Content
+        </Layout>
+      </Layout>
+    </Layout>
+  )
+}
+export default GeekLayout
